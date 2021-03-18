6375cf769ab951550cb532f909d09fcb428b64f7
Ray <ray@Rayde-MacBook-Pro-3.local>
2021-03-18 20:48:49 +0800

    更新版本

--- guessnumber.py
+++ guessnumber.py
@@ -1,14 +1,18 @@
 import random
 r = random.randint(1,100)
-answer = 25
+answer = 63
+count = 0
 while True:
+	count += 1
 	a = input('猜猜看數字是多少？')
 	a = float(a)
 	if a == answer:
 		print('恭喜猜對了')
+		print('這是你猜的第', count ,'次了 請再接再厲')
 		break
 	else:
-		if a < 25:
-			print('哈哈哈 妳猜錯了誒','比答案還小')
-		if a > 25:
-		    print('哈哈哈 妳猜錯了誒','比答案還大')
+		if a < 63:
+			print('哈哈 兄弟給力點 你猜錯了 猜那麼小幹嘛')
+		if a > 63:
+		    print('哈哈 你猜錯了 是想猜幾次啊 沒事別猜那麼大可以嗎')
+	print('這是你猜的第', count ,'次了誒加油好嘛')
